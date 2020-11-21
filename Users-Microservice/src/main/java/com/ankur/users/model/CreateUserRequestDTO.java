d2609a485e335381aca7156dfa8803fa9e8a6f17
Ankur <ankurdhami008@gmail.com>
2020-11-21 12:36:36 +0530

    Create User and validating request body
    Added create user method and added validation for request body

---
+++ Users-Microservice/src/main/java/com/ankur/users/model/CreateUserRequestDTO.java
@@ -0,0 +1,24 @@
+package com.ankur.users.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class CreateUserRequestDTO {
+
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must not be less than two characters")
+    private String firstName;
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 2, message = "Last name must not be less than two characters")
+    private String lastName;
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be equal or greater than 8 characters and less than 16 characters")
+    private String password;
+    @NotNull(message = "Email cannot be null")
+    @Email
+    private String email;
+}
